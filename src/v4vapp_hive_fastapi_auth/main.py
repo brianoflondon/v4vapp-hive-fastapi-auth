38119932ad433255588ab91733571071dd00541a
Brian of London (home imac) <brian@v4v.app>
2024-02-06 12:04:51 +0200

    Add CORS middleware and secure route for authenticated users
    Signed-off-by: Brian of London (home imac) <brian@v4v.app>

--- src/v4vapp_hive_fastapi_auth/main.py
+++ src/v4vapp_hive_fastapi_auth/main.py
@@ -1,10 +1,14 @@
 import logging
 from contextlib import asynccontextmanager
+from typing import Annotated
 
-from fastapi import FastAPI
+from fastapi import Depends, FastAPI
 from single_source import get_version
 
 from v4vapp_hive_fastapi_auth import routes
+from v4vapp_hive_fastapi_auth.helpers import get_current_active_user
+from v4vapp_hive_fastapi_auth.models import User
+from fastapi.middleware.cors import CORSMiddleware
 
 __version__ = get_version(__name__, "", default_return="0.0.1")
 
@@ -19,22 +23,30 @@ app = FastAPI(
     lifespan=lifespan,
     title="V4VApp Authenticated Hive API",
     description="Authenticated API for Hive.",
-    # version=__version__,  # type: ignore
-    # terms_of_service="http://example.com/terms/",
-    # contact={
-    #     "name": "Brian of London",
-    #     "url": "http://x-force.example.com/contact/",
-    #     "email": "dp@x-force.example.com",
-    # },
-    # license_info={
-    #     "name": "MIT",
-    #     "url": "https://www.apache.org/licenses/LICENSE-2.0.html",
-    # },
 )
 
 app.include_router(routes.router)
 
+origins = ["*"]
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=origins,
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 @app.get("/", tags=["v4vapp"])
 async def index():
     return {"message": "Hello World"}
+
+
+@app.get("/secure/", tags=["v4vapp"])
+async def secure(current_user: Annotated[User, Depends(get_current_active_user)]):
+
+    return {
+        "current_user": current_user.username,
+        "challenge": current_user.challenge,
+        "disabled": current_user.disabled,
+    }
